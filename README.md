d1572b034c5827e1cf83c160ff2b76a2c4ab8e0e
Kevin Simper <kevin.simper@gmail.com>
2020-09-22 23:15:20 +0200

    cli

---
+++ README.md
@@ -0,0 +1,11 @@
+# zod generate
+
+```
+$ cat ./test/input.js | node dist/cli.js
+z.object({
+  a: z.number(),
+  b: z.string(),
+  c: z.boolean(),
+});
+
+```
---
+++ src/cli.ts
@@ -0,0 +1,14 @@
+#!/user/bin/env node
+import { generate } from "./";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+let buffer = "";
+process.stdin.on("data", function (chunk) {
+  buffer += chunk;
+});
+process.stdin.on("end", function () {
+  const result = generate(buffer);
+  console.log(result);
+});
